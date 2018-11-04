7d36fbc06a6a2841a08dbd807e6411f5ed8bdd9b
Lev Vasilyev <wanbergg@gmail.com>
2018-11-04 23:49:58 +0300

    video and mobile devices sections
    working on next one

---
+++ exam-layout/scripts/script.js
@@ -0,0 +1,13 @@
+function btn(){
+    let video = document.getElementById("video");
+    videoPlay();
+    overlayHide();
+    function videoPlay(){
+        video.play();
+        video.setAttribute("controls","controls");
+    }
+
+    function overlayHide(){
+        document.getElementById("video__overlay").classList+=' hidden';
+    }
+}
